2d3a948ea8a7014443008099de8a5a519b1598a1
nicocaine <fncruz13@gmail.com>
2015-02-18 18:29:15 +0000

    google map engine

---
+++ google app engine/main.py
@@ -0,0 +1,15 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class AllHandler(webapp.RequestHandler):
+    def get(self):
+        self.redirect("http://radproject.byethost13.com/", True)
+
+application = webapp.WSGIApplication([('/.*', AllHandler)])
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
